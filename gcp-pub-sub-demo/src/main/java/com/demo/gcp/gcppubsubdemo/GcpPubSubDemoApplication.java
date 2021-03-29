34da8b7614585f21bb26da260d974bb5eb76cbf4
Sumit <sumitgupta28@gmail.com>
2021-03-29 14:57:54 -0400

    init

---
+++ gcp-pub-sub-demo/src/main/java/com/demo/gcp/gcppubsubdemo/GcpPubSubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.gcp.gcppubsubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpPubSubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpPubSubDemoApplication.class, args);
+	}
+
+}
